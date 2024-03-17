9e7fa8e87b8226aabac60a961261d6e61b5620d3
Artur Wyrozębski <wyrozebski.artur@gmail.com>
2024-03-17 22:27:11 +0100

    Add Id generation for DatabaseSeed in EFSeed.Core.Tests.Common.

--- tests/EFSeed.Tests.Common/DatabaseSeed.cs
+++ tests/EFSeed.Tests.Common/DatabaseSeed.cs
@@ -8,6 +8,7 @@ public class CustomDatabaseSeed : IDatabaseSeed
     public void Seed(SeedBuilder builder)
     {
         var countriesFaker = new Faker<Country>()
+            .RuleFor(c => c.Id, f => f.IndexFaker + 1)
             .RuleFor(c => c.Name, f => f.Address.Country());
         var countries = countriesFaker.Generate(5).ToList();
         builder.Add(countries);
