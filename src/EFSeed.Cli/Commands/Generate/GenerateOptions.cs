24402d976629f31ad1e59edf5ba09d89ad93b21f
Artur Wyrozębski <wyrozebski.artur@gmail.com>
2024-03-22 00:56:07 +0100

    Add shortnames to options in cli

--- src/EFSeed.Cli/Commands/Generate/GenerateOptions.cs
+++ src/EFSeed.Cli/Commands/Generate/GenerateOptions.cs
@@ -6,7 +6,7 @@ namespace EFSeed.Cli.Commands.Generate;
 [Verb("generate", HelpText = "Generates sql script." )]
 public class GenerateOptions : Options
 {
-    [Option("mode", HelpText = $"Generation mode to use. Available modes: insert, merge.", Required = true)]
+    [Option('m', "mode", HelpText = $"Generation mode to use. Available modes: insert, merge.", Required = true)]
     public GenerationMode Mode { get; set; }
     [Option("no-build", Required = false, HelpText = "Do not build the project before generating the script.")]
     public bool NoBuild { get; set; }
